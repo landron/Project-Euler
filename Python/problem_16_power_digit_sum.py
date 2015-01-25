67eadba319c316f2ff992286fc83fedc33497578
Liviu Andron <liviu.andron.work@gmail.com>
2015-01-25 03:10:38 +0100

    16: What is the sum of the digits of the number 21000?

---
+++ Python/problem_16_power_digit_sum.py
@@ -0,0 +1,49 @@
+"""
+    Problem 16 : Power digit sum
+    http://projecteuler.net/problem=16
+        What is the sum of the digits of the number 2^1000?
+    Version: 2015.01.25
+
+    pylint.bat --version
+        No config file found, using default configuration
+        pylint 1.4.0,
+        astroid 1.3.2, common 0.63.2
+        Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:15:05) [MSC v.1600 32 bit (Intel)]
+    Your code has been rated at 10.00/10
+"""
+
+from functools import reduce
+
+def multiply(number, factor):
+    """multiply large number by the given factor"""
+    number = [factor*i for i in number]
+    carry = 0
+    result = []
+    for i in reversed(number):
+        result += [(carry+i)%10]
+        carry = (carry+i) // 10
+    while carry:
+        result += [carry%10]
+        carry //= 10
+    result.reverse()
+    return result
+
+def debug_validations():
+    """all the assertions"""
+    assert 4662 == reduce(lambda x, y: 10*x+y, multiply([6, 6, 6], 7))
+    assert 81918 == reduce(lambda x, y: 10*x+y, multiply([6, 6, 6], 123))
+
+def problem_16():
+    """solve the problem, print the needed time"""
+
+    number = [1]
+    for i in range(1000):   #pylint: disable=unused-variable
+        number = multiply(number, 2)
+    result = sum(number)
+    assert 1366 == result
+    print("Result: {0}".format(result))
+
+if __name__ == "__main__":
+    debug_validations()
+
+    problem_16()
