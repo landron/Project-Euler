7e8de4049b2b25e906f6067c85fedccf72ab1e65
Liviu Andron <liviu.andron.work@gmail.com>
2017-10-16 22:32:15 +0200

    problem 28: found formula

--- Python/easy/problem_28_sum_diagonals.py
+++ Python/easy/problem_28_sum_diagonals.py
@@ -3,9 +3,23 @@
     https://projecteuler.net/problem=28
 
     https://www.hackerrank.com/contests/projecteuler/challenges/euler028
-        score:  25/100
-        todo_hackerrank : I bet there is a formula
+        done
+
+    Formula calculation (mostly in the subway):
+    - 1 + sum_of_odd(n*n+3*(n-1)*(n-1)+3) =>
+        sum_of_natural conversion, where 2*new_n+1 = old_n (old_n being odd)
+    - 1 + 16*sum(n*n) + 4*sum(n) + 4*n =>
+    - 1 + 2/3 * (8*n^3 + 15*n^2 + 13*n)
+        knowing that sum(n*n) = n(n+1)(2n+1)/6 and sum(n)=n(n+1)/2
+
+    Your code has been rated at 8.59/10 (previous run: 8.59/10, +0.00)
 '''
+
+def problem_solve_formula(size):
+    assert size%2 == 1
+
+    n = (size-1)//2
+    return 1 + 2 * (8*n*n*n + 15*n*n + 13*n) // 3
 
 def calculate_sum(tab):
     size = len(tab)
@@ -16,7 +30,7 @@ def calculate_sum(tab):
             sum_ += tab[i][size-i-1]
     return sum_
 
-def problem_solve(size):
+def problem_solve_brute(size):
     assert size%2 == 1
 
     tab = size * [[]]
@@ -60,24 +74,34 @@ def problem_solve(size):
 
     return calculate_sum(tab)
 
+def problem_solve(size):
+    # return problem_solve_brute(size)
+    return problem_solve_formula(size)
+
 # https://www.hackerrank.com/contests/projecteuler/challenges/euler028
-def parse_input():
-    T = int(input().strip())
-    for _ in range(T):
-        N = int(input().strip())
-        print(problem_solve(N))
+def parse_input_hackerrank():
+    cases = int(input().strip())
+    for _ in range(cases):
+        size = int(input().strip())
+
+        res = problem_solve(size)
+        # hackerrank condition
+        res = res%(10**9+7)
+        print(res)
 
 def problem():
     print(problem_solve(1001))
 
 def debug_assertions():
-    assert problem_solve(3) == 25
-    assert problem_solve(5) == 101
+    assert problem_solve_brute(3) == 25
+    assert problem_solve_brute(5) == 101
+    assert problem_solve_formula(3) == 25
+    assert problem_solve_formula(5) == 101
 
 def main():
     debug_assertions()
 
-    # parse_input()
+    # parse_input_hackerrank()
     problem()
 
 if __name__ == "__main__":
