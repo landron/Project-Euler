2a2a9e6729a2c9b0b9475d64af731eaed30e2da4
Liviu Andron <liviu.andron.work@gmail.com>
2018-04-15 20:01:01 +0200

    Problem 41: only 25/100 (even if easy)

--- Python/todo_hackerrank/problem_41_pandigital_prime.py
+++ Python/todo_hackerrank/problem_41_pandigital_prime.py
@@ -6,11 +6,24 @@
 
     https://www.hackerrank.com/contests/projecteuler/challenges/euler041
         solve_problem_generate is very fast
+        todo_hackerrank :   Score: 25.00 (2 ok, 3 timeouts)
+            my cache (collections based) is not good enough
+            neither the bisect based one
+
+            cannot precalculate: too many
+            optimization: verify that the numbers are pandigital and prime
+                solve_problem_generate_testthem => no gain
 
     pylint 1.8.1
+        Your code has been rated at 8.82/10.
+
+    tag_cache
 '''
 
 import math
+# import collections
+import bisect
+
 from proj_euler import get_primes
 from proj_euler import get_digits, number_of_digits
 from proj_euler import get_permutation_next, get_permutation_start
@@ -77,10 +90,33 @@ def solve_problem_generate(limit_digits, limit):
         limit_digits -= 1
     return ret
 
+# too slow for hackerrank
 def solve_problem_generate_reallimit(limit_in):
     limit_digits = number_of_digits(limit_in)
     return solve_problem_generate(limit_digits, limit_in)
 
+def solve_problem_generate_testthem(limit_in):
+    '''
+        desperate try to pass the hackerrank tests
+            just test for small enough numbers (instead of calculating based on the number of digits)
+    '''
+    no_digits = number_of_digits(limit_in)
+    max_factor = math.floor(math.sqrt(limit_in))
+
+    if limit_in > 7000000:
+        return solve_problem_generate_reallimit(limit_in)
+
+    i = limit_in
+    while i > 100:
+        digits = get_digits(i)
+        if is_pandigital(digits) and is_prime(i, max_factor):
+            return i
+        if len(digits) < no_digits:
+            no_digits -= 1
+            max_factor = math.floor(math.sqrt(i))
+        i -= 1
+
+    return -1
 
 def solve_problem_calculate_primes(limit):
     '''
@@ -116,11 +152,50 @@ def parse_input():
     '''
         https://www.hackerrank.com/contests/projecteuler/challenges/euler032
     '''
+    calculated = dict()
+    cache = []
+
     test_cases = int(input().strip())
     for _ in range(test_cases):
         limit = int(input().strip())
-        result = solve_problem_reallimit(limit)
+
+        # 10 digits (0 included is too slow)
+        if number_of_digits(limit) > 7:
+            limit = 10**7
+
+        found = False
+        pos = bisect.bisect_left(cache, limit)
+        if pos != len(cache):
+            after = cache[pos]
+            if calculated[after] == -1:
+                result = -1
+                found = True
+            elif pos and calculated[after] == calculated[cache[pos-1]]:
+                result = calculated[after]
+                found = True
+
+        if found:
+            # print("found in cache")
+            pass
+        else:
+            # result = solve_problem_reallimit(limit)
+            result = solve_problem_generate_testthem(limit)
         print(result)
+
+        if not found:
+            if limit not in calculated:
+                calculated[limit] = result
+                bisect.insort_left(cache, limit)
+            if result != -1:
+                if result not in calculated:
+                    calculated[result] = result
+                    bisect.insort_left(cache, result)
+                    # do not delete in the middle
+            else:
+                pos = bisect.bisect_left(cache, limit)
+                del cache[:pos]
+
+        # print(cache)
 
 def problem():
     return solve_problem_power10(9)
